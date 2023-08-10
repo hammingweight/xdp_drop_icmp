0342b72cef547783115bd86639f88274ce5409a2
CA Meijer <hammingweight@gmail.com>
2023-08-10 07:27:19 +0200

    Add comments.

--- build_attach.sh
+++ build_attach.sh
@@ -1,4 +1,8 @@
-
+# Extract the BPF Type Format (BTF) types for the kernel.
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
+
+# Compile to BPF byte code.
 clang -O2 -g -Wall -target bpf -c xdp_drop_icmp.c -o xdp_drop_icmp.o
+
+# Attach the filter to XDP events for the specified interface.
 sudo ip link set enp0s3 xdpgeneric obj xdp_drop_icmp.o sec xdp_drop
